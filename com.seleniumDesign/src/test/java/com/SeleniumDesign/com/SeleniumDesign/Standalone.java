ceade6a8dcf01d4c16f9f92740804900cb7d0e46
sidharth <sidh100arth@gmail.com>
2025-05-06 12:33:17 +0530

    latest file changes

--- com.seleniumDesign/src/test/java/com/SeleniumDesign/com/SeleniumDesign/Standalone.java
+++ com.seleniumDesign/src/test/java/com/SeleniumDesign/com/SeleniumDesign/Standalone.java
@@ -1,6 +1,22 @@
 package com.SeleniumDesign.com.SeleniumDesign;
 
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class Standalone {
 	
-	WebDriver driver = new ChromeDriver();
+	@Test
+	public void urlOpen() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/client");
+		driver.findElement(By.id("userEmail")).sendKeys("te100st@test.com");
+		driver.findElement(By.id("userPassword")).sendKeys("12345678@Aa");
+		driver.findElement(By.id("login")).click();
+		
+		}
+	
 }
